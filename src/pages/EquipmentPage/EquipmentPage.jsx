d828a91778b5e39a0e4f9635a6530e035c1b36ea
triin <triin.sikk@gmail.com>
2024-03-23 20:53:22 +1000

    double category clicking bug fix, show/hide full list

--- src/pages/EquipmentPage/EquipmentPage.jsx
+++ src/pages/EquipmentPage/EquipmentPage.jsx
@@ -5,19 +5,24 @@ import "./EquipmentPage.css"
 
 function EquipmentPage() {
     const [equipmentItems, setEquipmentItems] = useState([])
-    // const [showEquipment, setShowequipment] = useState(false)
+    const [showEquipment, setShowEquipment] = useState(false)
 
     function addEquipment (equipment) {
         setEquipmentItems([...equipmentItems, equipment ])
     };
 
+    const _toggleFullEquipmentList = () => {
+        setShowEquipment(!showEquipment)
+    }
+
     return (
-        <div>
-            <h1>Manage your equipment</h1>
-            <div className="equipment-container">
-                <div></div>
+        <div className="equipment-container">
+            <h1 className="item-a">Manage your equipment</h1>
+            <div className="item-b">
                 <NewEquipmentForm addEquipment={addEquipment}/>
-                <EquipmentList equipmentItems={equipmentItems} />
+            </div>
+            <div className="item-c">
+                <button onClick={_toggleFullEquipmentList} >{showEquipment ? "Hide full equipment list" : "Show full equipment list"}</button> {showEquipment && <EquipmentList equipmentItems={equipmentItems} />}
             </div>
         </div>
     )
