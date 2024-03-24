b0d7e43de4de720941fddceda8720d4a09b0b698
EmmaZhang <emmazxd@gmail.com>
2024-03-24 18:26:02 +1100

    fix journalDetail page bug

--- src/pages/JournalFormPage/JournalFormPage.jsx
+++ src/pages/JournalFormPage/JournalFormPage.jsx
@@ -1,15 +1,11 @@
 import { createJournal } from "../../utilities/journals-api";
 import JournalForm from "../../components/JournalForm/JournalForm";
-import { useJournalContext } from "../JournalPage/JournalContext";
 
 export default function JournalFormPage() {
-  const { addJournalToList } = useJournalContext();
-
   async function addJournal(journal) {
     try {
       //send to Backend
       await createJournal(journal);
-      addJournalToList(journal);
     } catch (err) {
       console.log("create jouranl", err);
     }
