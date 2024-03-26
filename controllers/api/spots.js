2558a93389eb8dd5a82fcf228caf0d3b89701755
lguerrera <laurenguerrera@gmail.com>
2024-03-26 16:48:03 +1100

    slightly debugged spot functionality. to be merged with hike page

--- controllers/api/spots.js
+++ controllers/api/spots.js
@@ -1,16 +1,29 @@
 const Hike = require('../../models/hike');
 
 module.exports = {
+    index, 
     createSpot
 }
 
-async function createSpot(req, res) {
-    const hike = await Hike.findById(req.params.id);
-    // hike.spots.push(req.body);
+async function index(req, res) {
     try {
+        const hikes = await Hike.find({user: req.user._id});
+        res.json(hikes);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+}
+
+async function createSpot(req, res) {
+    // hike.spots.push(req.body);
+    console.log(req.body);
+    try {
+        const hike = await Hike.create({title: req.body.name, user: req.user._id});
+        console.log(hike);
         res.json(hike);
         // await hike.save();
     } catch (err) {
+        console.log(err);
         res.status(400).json(err);
     }
 }
