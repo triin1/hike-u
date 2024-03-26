f9b3c0a4ffdf8551792d09c0c7cde518a649d38a
EmmaZhang <emmazxd@gmail.com>
2024-03-26 16:29:59 +1100

    add create sucessful notification

--- src/pages/JournalFormPage/JournalFormPage.jsx
+++ src/pages/JournalFormPage/JournalFormPage.jsx
@@ -1,11 +1,17 @@
 import { createJournal } from "../../utilities/journals-api";
 import JournalForm from "../../components/JournalForm/JournalForm";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function JournalFormPage() {
   async function addJournal(journal) {
     try {
       //send to Backend
       await createJournal(journal);
+
+      toast.success("Success Notification !", {
+        position: "top-center",
+      });
     } catch (err) {
       console.log("create jouranl", err);
     }
