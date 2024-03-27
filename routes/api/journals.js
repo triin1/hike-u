7e53f23bd69be249e578e8574f4c0ac47073ebbe
EmmaZhang <emmazxd@gmail.com>
2024-03-27 17:14:29 +1100

    add ensureLogin on journals

--- routes/api/journals.js
+++ routes/api/journals.js
@@ -3,21 +3,26 @@ const router = express.Router();
 const journalsCtrl = require("../../controllers/api/journals");
 const upload = require("../../utils/multer");
 
-// const ensureLoggedIn = require("../../config/ensureLoggedIn");
+const ensureLoggedIn = require("../../config/ensureLoggedIn");
 
 // POST /api/journals/new
-router.post("/new", upload.single("image"), journalsCtrl.create);
+router.post(
+  "/new",
+  upload.single("image"),
+  ensureLoggedIn,
+  journalsCtrl.create
+);
 
 // GET /api/journals
 router.get("/", journalsCtrl.index);
 
 // DELETE /api/journals/:id
-router.delete("/:id", journalsCtrl.delete);
+router.delete("/:id", ensureLoggedIn, journalsCtrl.delete);
 
 // GET /api/journals/:id
 router.get("/:id", journalsCtrl.show);
 
 // PUT /api/journals/:id/edit
-router.put("/:id", journalsCtrl.update);
+router.put("/:id", ensureLoggedIn, journalsCtrl.update);
 
 module.exports = router;
