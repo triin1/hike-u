cdded86bd78010a90de11cef303aa3698dcf54b9
lguerrera <laurenguerrera@gmail.com>
2024-03-27 16:37:12 +1100

    spots functionality is fixed woohoooo

--- controllers/api/hikes.js
+++ controllers/api/hikes.js
@@ -1,6 +1,7 @@
 const Hike = require('../../models/hike');
 
 async function create(req, res) {
+    console.log(req.body);
     try {
         const {distance, 
             startLocation, 
@@ -23,7 +24,13 @@ async function create(req, res) {
             equipments, 
             spots,
             user: user._id
-        })
+        });
+        console.log(hike);
+       // hike.spots.push(req.body.spots);
+        // await hike.save();
+        // console.log(hike);
+
+        res.json(hike);
         
     } catch (err) {
         res.status(400).json(`Creating Hike error on database: ${err}`)
@@ -31,7 +38,7 @@ async function create(req, res) {
 
     
     
-    console.log(req.body, req.user)
+   // console.log(req.body, req.user)
 }
 
 module.exports = {
