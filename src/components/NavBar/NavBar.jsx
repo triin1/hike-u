2b5688a6fb2e4783d8d78177322d1e0a252def28
EmmaZhang <emmazxd@gmail.com>
2024-03-27 11:16:16 +1100

    add logo on navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -23,8 +23,12 @@ const NavBar = ({ user, setUser }) => {
         href="/"
         className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
       >
-        <svg className="bi pe-none me-2" width="40" height="32"></svg>
-        <span className="fs-2 fw-bold">Hike-U</span>
+        <img
+          src="./images/hikeU.png"
+          alt="Hike-u logo"
+          className="hikeu img-fluid"
+        />
+        {/* <span className="fs-2 fw-bold">Hike-U</span> */}
       </a>
       <hr />
       <button className="sidebar-toggle" onClick={toggleSidebar}>
