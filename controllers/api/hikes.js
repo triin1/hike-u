3f01feece6835059a14ece5de0b7d7b8f1e4fbe6
Yuchen Liu <lyc990312@hotmail.com>
2024-03-27 13:14:05 +1000

    Modified the hike page

--- controllers/api/hikes.js
+++ controllers/api/hikes.js
@@ -1,7 +1,37 @@
 const Hike = require('../../models/hike');
 
 async function create(req, res) {
-    console.log(req.body)
+    try {
+        const {distance, 
+            startLocation, 
+            endLocation, 
+            title, 
+            description, 
+            startDate, 
+            endDate, 
+            equipments, 
+            spots} = req.body
+        const user = req.user
+        const hike = await Hike.create({
+            distance,
+            startLocation, 
+            endLocation, 
+            title, 
+            description, 
+            startDate, 
+            endDate, 
+            equipments, 
+            spots,
+            user: user._id
+        })
+        
+    } catch (err) {
+        res.status(400).json(`Creating Hike error on database: ${err}`)
+    }
+
+    
+    
+    console.log(req.body, req.user)
 }
 
 module.exports = {
